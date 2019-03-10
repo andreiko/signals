f4801ac2530ffabc762187a6a5d02f8630ab48a3
Andrey Bulgakov <mail@andreiko.ru>
2019-03-10 16:03:12 -0700

    XYZ -> CSV

--- main.c
+++ main.c
@@ -1,154 +1,147 @@
-#include <avr/io.h>
 #include <avr/interrupt.h>
 
-#include "frame.h"
-#include "animations.h"
+// ADC references and inputs
+#define ADMUX_REFS_INTERNAL11V ((uint8_t)0b11)
+#define ADMUX_MUX_ACCX ((uint8_t)0b0111)
+#define ADMUX_MUX_ACCY ((uint8_t)0b0000)
+#define ADMUX_MUX_ACCZ ((uint8_t)0b0001)
+// ADC prescaler
+#define ADCSRA_ADPS_8 ((uint8_t)0b011)
 
-// SIDES
-#define SIDES_PORT PORTC
-#define SIDES_DDR DDRC
-
-// MATRIX
-#define MATRIX_PORT PORTD
-#define MATRIX_DDR DDRD
-#define MATRIX_EN 0
-#define MATRIX_DATA 1
-#define MATRIX_SHIFT 2
-#define MATRIX_STORE 3
-
-volatile Animation *activeAnimation = 0;
-volatile uint8_t activeFrame;
-volatile uint8_t activeRow;
-volatile uint8_t frameExposure;
-
-void sides_init() {
-    SIDES_DDR |= 0b111111;
-    SIDES_PORT &= ~(0b111111);
-}
-
-void matrix_init() {
-    MATRIX_DDR |= 1 << MATRIX_EN | 1 << MATRIX_DATA | 1 << MATRIX_SHIFT | 1 << MATRIX_STORE;
-    MATRIX_PORT &= ~(1 << MATRIX_EN | 1 << MATRIX_DATA | 1 << MATRIX_SHIFT | 1 << MATRIX_STORE);
-}
-
-void matrix_shift(uint8_t value) {
-    for (int i = 0; i < 8; ++i) {
-        if (value & (1 << i)) {
-            MATRIX_PORT |= 1 << MATRIX_DATA;
-        } else {
-            MATRIX_PORT &= ~(1 << MATRIX_DATA);
+uint8_t format_uint16(uint16_t input, volatile uint8_t *output) {
+    uint8_t written = 0;
+    for (uint16_t i = 10000; i > 0; i = i / (uint16_t) 10) {
+        uint8_t d = (uint8_t) (input / i);
+        if (d > 0 || written > 0) {
+            output[written++] = (uint8_t) '0' + d;
         }
-        MATRIX_PORT |= 1 << MATRIX_SHIFT;
-        MATRIX_PORT &= ~(1 << MATRIX_SHIFT);
+        input = input % i;
     }
-}
 
-void matrix_store() {
-    MATRIX_PORT |= 1 << MATRIX_STORE;
-    MATRIX_PORT &= ~(1 << MATRIX_STORE);
-}
-
-// SWITCH
-
-#define SW_DDR DDRD
-#define SW_INP PIND
-#define SW_PORT PORTD
-#define SW_INPUT_LEFT 6
-#define SW_INPUT_RIGHT 7
-
-#define SW_POS_UNDEFINED 0
-#define SW_POS_MIDDLE 1
-#define SW_POS_LEFT 2
-#define SW_POS_RIGHT 3
-
-volatile uint8_t currentSwitch = SW_POS_UNDEFINED;
-
-void sw_init() {
-    SW_DDR &= ~(1 << SW_INPUT_LEFT | 1 << SW_INPUT_RIGHT);
-    SW_PORT |= 1 << SW_INPUT_LEFT | 1 << SW_INPUT_RIGHT;
-}
-
-void sw_update() {
-    uint8_t inp = SW_INP;
-    if ((inp & (1 << SW_INPUT_LEFT)) == 0) {
-        if (currentSwitch != SW_POS_LEFT) {
-            currentSwitch = SW_POS_LEFT;
-
-            activeAnimation = &turningLeft;
-            activeFrame = 0;
-            activeRow = 0;
-            frameExposure = 0;
-        }
-    } else if ((inp & (1 << SW_INPUT_RIGHT)) == 0) {
-        if (currentSwitch != SW_POS_RIGHT) {
-            currentSwitch = SW_POS_RIGHT;
-
-            activeAnimation = &turningRight;
-            activeFrame = 0;
-            activeRow = 0;
-            frameExposure = 0;
-        }
-    } else if (currentSwitch != SW_POS_MIDDLE) {
-        currentSwitch = SW_POS_MIDDLE;
-
-        activeAnimation = &dot;
-        activeFrame = 0;
-        activeRow = 0;
-        frameExposure = 0;
+    if (!written) {
+        output[written++] = '0';
     }
+
+    return written;
 }
 
-void start_timer() {
-    TCCR0A = 2; // GWM = CTC
-    TCCR0B = 3; // prescaler: IOclk(1MHz) / 64
-    OCR0A = 17; // 920 Hz / 9 = 102 Hz / scan
+volatile struct {
+    enum {
+        SLEEPING,
+        MEASURING_X,
+        MEASURING_Y,
+        MEASURING_Z,
+        SENDING
+    } id;
+    struct {
+        uint16_t x;
+        uint16_t y;
+        uint16_t z;
+    } data;
+    uint8_t buffer[16];
+    uint8_t pos;
+    uint8_t slept;
+} state;
 
-    // enable TIMER0_COMPA interrupt
-    TIMSK0 = 1 << OCIE0A;
-    // enable interrupts
+void state_init() {
+    state.id = SLEEPING;
+    state.data.x = 0;
+    state.data.y = 0;
+    state.data.z = 0;
+    state.pos = 0;
+}
+
+void uart_init() {
+    const uint16_t baud_setting = (F_CPU / 4 / 9600 - 1) / 2;;
+    UBRR0H = (uint8_t) (baud_setting >> 8);
+    UBRR0L = (uint8_t) baud_setting;
+    UCSR0A |= 1 << U2X0;
+    UCSR0B |= 1 << TXEN0;
+}
+
+void adc_init() {
+    // prescaler: 1Mhz / 8 = 125kHz
+    ADCSRA = 1 << ADEN | 1 << ADIE | ADCSRA_ADPS_8 << ADPS0;
+}
+
+void timer_init() {
+    // CTC
+    TCCR0A = 0b10;
+    // (1 / 7812.5Hz) * 156 => 20ms
+    OCR0A = 156;
+    // 8Mhz / 1024 = 7812.5Hz
+    TCCR0B = 0b101;
+
+    TIMSK0 |= 1 << OCIE0A;
+}
+
+void adc_start(uint8_t input) {
+    // choose input
+    ADMUX = ADMUX_REFS_INTERNAL11V << REFS0 | input << MUX0;
+    // start conversion
+    ADCSRA |= 1 << ADSC;
+}
+
+int main() {
+    state_init();
+    adc_init();
+    uart_init();
+    timer_init();
     sei();
+    for (;;);
+}
+
+ISR(USART0_TX_vect) {
+    state.id = SLEEPING;
+}
+
+ISR(USART0_UDRE_vect) {
+    if (state.buffer[state.pos] != 0) {
+        UDR0 = state.buffer[state.pos++];
+    } else {
+        UCSR0B &= ~(1 << UDRIE0);
+    }
+}
+
+// ADC conversion complete
+ISR(ADC_vect) {
+    uint8_t low = ADCL;
+    uint16_t result = (ADCH << 8) + low;
+
+    switch (state.id) {
+        case MEASURING_X:
+            state.data.x = result;
+            state.id = MEASURING_Y;
+            adc_start(ADMUX_MUX_ACCY);
+            break;
+        case MEASURING_Y:
+            state.data.y = result;
+            state.id = MEASURING_Z;
+            adc_start(ADMUX_MUX_ACCZ);
+            break;
+        case MEASURING_Z:
+            state.data.z = result;
+            state.id = SENDING;
+
+            state.pos = format_uint16(state.data.x, state.buffer);
+            state.buffer[state.pos++] = ',';
+            state.pos += format_uint16(state.data.y, state.buffer + state.pos);
+            state.buffer[state.pos++] = ',';
+            state.pos += format_uint16(state.data.z, state.buffer + state.pos);
+            state.buffer[state.pos++] = '\n';
+            state.buffer[state.pos] = 0;
+            state.pos = 0;
+
+            // enable UART interrupts
+            UCSR0B |= 1 << TXCIE0 | 1 << UDRIE0;
+
+            break;
+        default:
+            break;
+    }
 }
 
 ISR(TIMER0_COMPA_vect) {
-    sw_update();
-
-    if (activeAnimation == 0) {
-        return;
-    }
-
-    uint8_t hi = 0, lo = ~(activeAnimation->frames[activeFrame].rows[activeRow]) & 0b11111;
-    if (activeRow > 0) {
-        hi = 1 << (activeRow - 1);
-    } else {
-        lo |= 1 << 7;
-    }
-
-    matrix_shift(lo);
-    matrix_shift(hi);
-    matrix_store();
-
-    SIDES_PORT = activeAnimation->frames[activeFrame].sides;
-
-    if (++activeRow >= FRAME_ROWS) {
-        activeRow = 0;
-
-        if (++frameExposure >= activeAnimation->frames[activeFrame].duration) {
-            frameExposure = 0;
-
-            if (++activeFrame >= activeAnimation->count) {
-                activeFrame = 0;
-            }
-        }
-    }
-}
-
-int main(void) {
-    sw_init();
-    sides_init();
-    matrix_init();
-
-    activeAnimation = &dot;
-    start_timer();
-
-    for (;;) {}
+    state.id = MEASURING_X;
+    adc_start(ADMUX_MUX_ACCX);
 }
